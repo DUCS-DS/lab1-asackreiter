abd6d6ea99d9c8f5c0231785f4cddbac2692405a
asackreiter <asackreiter@drury.edu>
2025-01-22 11:14:10 -0600

    Added code and author info

--- edit_me.py
+++ edit_me.py
@@ -1,12 +1,13 @@
-
+#Lab 1
+#Author: Artemis Sackreiter
 def find_max(lst):
     """return the maximum element"""
 
     current_max = lst[0]
 
-    #
-    #  YOUR CODE GOES HERE
-    #
+    for i in lst:
+        if  (lst[i] > current_max):
+            current_max = lst[i] 
 
     return current_max
 
